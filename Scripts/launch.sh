a954d5e76a388f59f32a250997debc00fd06f706
Stephane REYTAN <srnfr@users.noreply.github.com>
2024-04-27 13:06:06 +0200

    Update launch.sh

--- Scripts/launch.sh
+++ Scripts/launch.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -x
 #Usage: ./launch.sh <target URL> <Index number - for repeated tests>
 
 sudo ifconfig ingress mtu 296
@@ -7,7 +9,7 @@ echo "0 0 0" > ../Data/windows.csv
 
 echo "0 0" > ../Data/buff.csv
 
-for i in {1..100}
+for i in {1..10}
 do
 
 	sudo iptables -I INPUT -p tcp -d 100.64.0.2 -m state --state ESTABLISHED -j NFQUEUE --queue-num 0
@@ -15,6 +17,7 @@ do
 	sudo ./prober "$1" 8000 5000 1000 >> ../Data/buff.csv
 	sleep 2
 	rm -f index*
+ 	rm -f *MB.bin*
 	sudo iptables --flush
 	sleep 3
 done
